76d706ea9c03ef617478d723a550da6679b60dba
amir arsalan <amirarsalannazanin6@gmail.com>
2023-02-04 00:53:21 +0330

    create app Accounts ! (Not completed)

---
+++ accounts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import UserRegister
+
+app_name = 'accounts'
+urlpatterns = [
+    path('register', UserRegister.as_view(), name='register')
+]
---
+++ accounts/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+
+
+class UserRegister(APIView):
+    def get(self):
+        pass
