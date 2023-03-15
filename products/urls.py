ef83f49917232a422a70c41a7658aba9dfdd17e8
amir arsalan <amirarsalannazanin6@gmail.com>
2023-03-15 15:00:45 +0330

    products / CRUD commends and Replies .

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,15 @@ from . import views
 app_name = 'products'
 urlpatterns = [
     path('', views.ProductView.as_view(), name='all_products'),
+    # list All products
     path('categories/', views.CategoriesView.as_view(), name='all_categories'),
-    path('subcategories/<int:id>/', views.SubCategories.as_view(), name='sub_categories')
+    # list of categories
+    path('subcategories/<int:id>/', views.SubCategoriesView.as_view(), name='sub_categories'),
+    # Subcategory list based on ID
+    path('comments/<int:id>/', views.CommentProductView.as_view(), name='all_comment'),
+    # comments list based on ID Product
+    path('add_comment/<int:id>/', views.CreationCommentView.as_view(), name='add_comment'),
+    # creation a comment by ID product
+    path('add_reply/', views.CreationReplyView.as_view(), name='add_reply'),
+    # creation a reply by ID comment
 ]
