370067b6cb27c4769e4a6a46205ddb9b0bd3bfb0
amir arsalan <amirarsalannazanin6@gmail.com>
2023-04-27 16:37:30 +0330

    test

--- admin_site/serialisers.py
+++ admin_site/serialisers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
-
+from lib.DiscountCalculator import discount_calculator, decrease_percentage
+from lib.find_Date import find_date
 from accounts.models import User
-from products.models import Genre, ProductModel
+from products.models import Genre, ProductModel, DiscountModel
 
 from django.shortcuts import get_object_or_404
 
@@ -46,7 +47,6 @@ class CreationProductSerializers(serializers.Serializer):
         price = validated_data['price']
         user = User.objects.get(id=validated_data['user'])
         category = get_object_or_404(Genre, name=validated_data['category'])
-
         product = ProductModel.objects.create(user=user, title=title, description=description,
                                               price=price)
         product.category.add(category.id, category.parent.id)
@@ -54,4 +54,31 @@ class CreationProductSerializers(serializers.Serializer):
         return None
 
 
+class CreationDiscountSerializers(serializers.Serializer):
+    product_id = serializers.IntegerField()
+    percent = serializers.IntegerField(required=False)
+    endprice = serializers.IntegerField(required=False)
+    expired = serializers.CharField()
 
+    def create(self, validated_data):
+        try:
+            try:
+                if validated_data['percent']:
+                    product = ProductModel.objects.get(id=validated_data['product_id'])
+                    expired = find_date(validated_data['expired'])
+                    end_price = discount_calculator(product.price, discount_percent=validated_data['percent'])
+                    return DiscountModel.objects.create(product=product, percent=validated_data['percent'],
+                                                        endprice=int(end_price),
+                                                        expired=expired)
+            except:
+
+                if validated_data['endprice']:
+                    product = ProductModel.objects.get(id=validated_data['product_id'])
+                    percent = decrease_percentage(p1=product.price, p2=validated_data['endprice'])
+                    expired = find_date(validated_data['expired'])
+                    return DiscountModel.objects.create(product=product, percent=percent,
+                                                        endprice=validated_data['endprice'],
+                                                        expired=expired)
+        except:
+            if validated_data['percent'] & validated_data['endprice']:
+                raise ValueError('You can only send one of the two items percent or endprice !!')
